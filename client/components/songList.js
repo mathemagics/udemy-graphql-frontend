0e20c7dc7560f518f947cf3cb1990f8dacc47e10
Paul WB <higgsbison@gmail.com>
2017-02-18 11:20:14 -0800

    add render songs method

--- client/components/songList.js
+++ client/components/songList.js
@@ -3,11 +3,22 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
 class songList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map( song => {
+      return (
+        <li key={song.title}>
+          {song.title}
+        </li>
+      )
+    })
+  }
     render() {
       console.log(this.props);
       return (
         <div>
-          SongList
+          {this.props.data.loading ?
+            <div>loading...</div> :
+            <ul> {this.renderSongs()} </ul>}
         </div>
       );
     }
