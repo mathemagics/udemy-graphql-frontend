78de565deaaa91adf2492dce73dd915e648c1c0e
Paul WB <higgsbison@gmail.com>
2017-02-19 14:36:25 -0800

    add lyric list component

--- client/components/songDetail.js
+++ client/components/songDetail.js
@@ -3,6 +3,7 @@ import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
 import fetchSong from '../queries/fetchSong';
 import LyricCreate from './lyricCreate';
+import LyricList from './lyricList';
 
 class SongDetail extends Component {
   render() {
@@ -12,6 +13,7 @@ class SongDetail extends Component {
       <div>
         <Link to="/">Back</Link>
       { song && <h3>{song.title}</h3> }
+      <LyricList />
       <LyricCreate songId={id}/>
       </div>
     );
