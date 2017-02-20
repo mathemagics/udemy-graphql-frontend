674f7f66dd3ee4d22043cc467de593b7c3176945
Paul WB <higgsbison@gmail.com>
2017-02-19 16:28:28 -0800

    connect 'like lyric' mutation and style likes

--- client/components/lyricList.js
+++ client/components/lyricList.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 class LyricList extends Component {
   onLike(id) {
-    console.log(id);
+    this.props.mutate({ variables: { id } });
   }
-
   renderLyrics() {
-    return this.props.lyrics.map( ({ id, content }) => {
+    return this.props.lyrics.map( ({ id, content, likes }) => {
       return (
         <li key={id} className="collection-item">
           {content}
-          <i className="material-icons" onClick={() => this.onLike(id)}>
-            thumb_up</i>
+          <div className="vote-box">
+            <i className="material-icons" onClick={() => this.onLike(id)}>
+              thumb_up
+            </i>
+            {likes}
+          </div>
         </li>
       );
     })
@@ -34,4 +39,4 @@ const mutation = gql`
   }
 `
 
-export default LyricList;
+export default graphql(mutation)(LyricList);
